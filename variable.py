4283779b885f8709455b30b38cc87e575d2632ca
dev <agrawaldev12345@gmail.com>
2025-11-23 15:22:45 +0530

    first commit

---
+++ variable.py
@@ -0,0 +1,15 @@
+n = int(input("enter the number :"))
+for i in range(1, n+1):
+    print(" "*(n-1), end=" ")
+    print("*"* (2*i-1), end=" ")
+    print("")
+
+
+n = int(input("enter te number :"))
+def add(n):
+    return n*n
+
+print(add(n))
+
+name = "abhishek"
+names = [1,2,3,4]
